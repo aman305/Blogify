579705c07e3af5c9732d59f1748eff3b1725dd45
amn305 <amanraj.ar206@gmail.com>
2024-08-22 11:42:41 +0530

    add blogs

--- index.js
+++ index.js
@@ -4,23 +4,27 @@ const mongoose = require('mongoose');
 const userRoute = require('./routes/user');
 const blogRoute = require('./routes/blog');
 const cookieParser = require('cookie-parser');
+const Blog = require('./models/blog');
 const checkForAuthenticationCookie = require('./middlewares/authentication');
 mongoose
   .connect('mongodb://localhost:27017/blogify')
   .then((e) => console.log('MongoDB connected'));
 
 const app = express();
-const PORT = 8000;
+const PORT = 8001;
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('./views'));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(checkForAuthenticationCookie('token'));
+app.use(express.static(path.resolve('./public')));
 app.use('/user', userRoute);
 app.use('/blog', blogRoute);
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  const allBlogs = await Blog.find({});
   res.render('home', {
     user: req.user,
+    blogs: allBlogs,
   });
 });
 
--- routes/blog.js
+++ routes/blog.js
@@ -1,5 +1,20 @@
 const { Router } = require('express');
+const multer = require('multer');
+const path = require('path');
+const Blog = require('../models/blog');
 const router = Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(`public/uploads`));
+  },
+  filename: function (req, file, cb) {
+    const filename = `${Date.now()}-${file.originalname}`;
+    cb(null, filename);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.get('/add-new', (req, res) => {
   return res.render('addBlog', {
@@ -7,5 +22,14 @@ router.get('/add-new', (req, res) => {
   });
 });
 
-router.post('/', (req, res) => {});
+router.post('/', upload.single('coverImage'), async (req, res) => {
+  const { title, body } = req.body;
+  const blog = await Blog.create({
+    body,
+    title,
+    createdBy: req.user._id,
+    coverImageURL: `uploads/${req.file.filename}`,
+  });
+  return res.redirect(`/blog/${blog._id}`);
+});
 module.exports = router;
